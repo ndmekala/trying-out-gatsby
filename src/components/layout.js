84b02ebe06ed4d4ffe8bda69ca9a807899cf3c36
Nirmal Mekala <ndmekala@icloud.com>
2021-06-29 21:00:32 -0500

    styling with tailwind

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,16 +3,16 @@ import { Link } from 'gatsby'
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <main>
-        <div className="bg-gray-500 rounded-xl p-8">
+    <main className="flex justify-center">
+        <div className="bg-gray-100 rounded-xl p-8 w-96">
         <title>{pageTitle}</title>
         <nav>
-            <ul>
+            <ul className="font-medium text-cyan-600">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
             </ul>
         </nav>
-        <h1>{pageTitle}</h1>
+        <h1 className="mb-1 mt-4 text-xl font-semibold">{pageTitle}</h1>
         {children}
 
         </div>
