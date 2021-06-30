bd5eb620b4e0bd7a2dca660fd644e8eddb664027
Nirmal Mekala <ndmekala@icloud.com>
2021-06-30 15:08:44 -0500

    adding title via graphql

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,22 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`)
+
   return (
     <main className="flex justify-center">
         <div className="bg-gray-100 rounded-xl mt-8 p-8 w-96">
         <title>{pageTitle}</title>
+        <p className="text-center mb-4">{data.site.siteMetadata.title}</p>
         <nav>
             <ul>
                 <li><Link to="/">Home</Link></li>
